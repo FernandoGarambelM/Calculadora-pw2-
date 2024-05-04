c8335e82ad765430fd71e0562e15fc206bb6ff47
nuevo_63 <ajrahuacso@gmail.com>
2024-05-04 13:36:19 -0500

    Primer commit, resta prueba

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+    int add(int a, int b){ return 0; }
+
+    int sub(int a, int b){
+        int resultado = a - b;
+        return resultado; 
+    }
+
+    int mul(int a, int b){ return 0; }
+
+    int div(int a, int b){ return 0; }
+
+    int mod(int a, int b){ return 0; }
+
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        Calculator calculadora = new Calculator();
+        
+
+        //RESTA
+        int resta = calculadora.sub(1, 3);
+        System.out.println("El resultado es: " + resta); 
+    }
+}

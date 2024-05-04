3701e43bcae4e88081f3f854abc00a279c4fbdb0
nuevo_63 <ajrahuacso@gmail.com>
2024-05-04 14:44:14 -0500

    Se soluciona errores de main, y se termina el primer comando (RESTA)

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,34 @@
+import java.util.*;
 public class Calculator {
+    Scanner sc = new Scanner(System.in);
+
+    //SUMAR
     int add(int a, int b){ return 0; }
 
     
-    //SUMAR
-    public void sumar(){
-        
-    }
-
+    //RESTAR
     int sub(int a, int b){
         int resultado = a - b;
         return resultado; 
     }
+    public void restar(){
+        System.out.println("Ingrese los dos numeros a restar: ");
+        System.out.print("Numero 1: ");
+        int a = sc.nextInt();
+        System.out.print("Numero 2: ");
+        int b = sc.nextInt();
+        int resultado = sub(a, b);
+        System.out.println("El resultado de la resta es: " + resultado + "\n");
+    }
+    
 
+    //MULTIPLICACION
     int mul(int a, int b){ return 0; }
 
+    //DIVIDIR
     int div(int a, int b){ return 0; }
 
+    //MODULO
     int mod(int a, int b){ return 0; }
 
 }
--- Main.java
+++ Main.java
@@ -7,19 +7,19 @@ class Main {
 
         while(menuActivo){
             opciones();
-            System.out.println("Ingrese su elección: ");
+            System.out.println("\nIngrese su elección: ");
             int seleccion = sc.nextInt();
             ejecutarSeleccion(seleccion);
         }
     }
 
     public static void opciones(){
-        System.out.println("OPCIONES DELA CALCULADORA:" + 
-        "1. SUMA " +
-        "2. RESTA " +
-        "3. MULTIPLICACION " +
-        "4. DIVISION " +
-        "5. MODULO "+
+        System.out.println("\nOPCIONES DELA CALCULADORA: \n" + 
+        "1. SUMA \n" +
+        "2. RESTA \n" +
+        "3. MULTIPLICACION \n" +
+        "4. DIVISION \n" +
+        "5. MODULO \n"+
         "6. SALIR");
     }
 
@@ -28,15 +28,15 @@ class Main {
         switch (x) {
             case 1: 
                 break;
-            case 2: 
-                break;
+            case 2: calculadora.restar();
+                break; 
             case 3: 
                 break;
             case 4: 
                 break;
             case 5: 
                 break;
-            case 6: calculadora.sumar(); 
+            case 6:  
                 break;
             
             default: System.out.println("El número igresado es incorrecto");
